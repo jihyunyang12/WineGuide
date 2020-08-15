3e61723118e462dca81201d7653a4e15a566b68d
jihyunyang12 <jihyun.yang21@gmail.com>
2020-08-14 20:49:37 -0400

    CORS enabled

--- WineGuide/Startup.cs
+++ WineGuide/Startup.cs
@@ -26,6 +26,14 @@ namespace WineGuide
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +43,8 @@ namespace WineGuide
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
